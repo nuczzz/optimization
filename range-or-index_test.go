56707fcb100c2818fe073bb682c2dceca2727f91
nuczzz <nuc_zzz@163.com>
2019-02-05 09:32:28 +0800

    add range/index benchmark test file

---
+++ range-or-index_test.go
@@ -0,0 +1,43 @@
+package optimization
+
+import "testing"
+
+const TraversalMax = 2<<10
+
+func initList() []string {
+	s := make([]string, TraversalMax)
+	for i := 0; i < TraversalMax; i++ {
+		s[i] = "https://github.com"
+	}
+	return s
+}
+
+func rangeTraversal(list []string) {
+	for i, data := range list {
+		_, _ = i, data
+	}
+}
+
+func indexTraversal(list []string) {
+	for i := 0; i < len(list); i++ {
+		_, _ = i, list[i]
+	}
+}
+
+// go test -run=^^$ -bench="^BenchmarkRangeTraversal$" -benchmem
+func BenchmarkRangeTraversal(b *testing.B) {
+	list := initList()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rangeTraversal(list)
+	}
+}
+
+// go test -run=^^$ -bench="^BenchmarkIndexTraversal$" -benchmem
+func BenchmarkIndexTraversal(b *testing.B) {
+	list := initList()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		indexTraversal(list)
+	}
+}
